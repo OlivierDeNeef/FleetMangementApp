e07adc5eb2f936a1094aa55a9a8db10b7a747cb5
Olvier De Neef <Olivier.dn.1999@gmail.com>
2021-10-01 14:39:07 +0200

    Added TankkaartException

---
+++ Domain/Exceptions/TankkaartException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DomainLayer.Exceptions
+{
+    public class TankkaartException : Exception
+    {
+        public TankkaartException()
+        {
+            
+        }
+
+        public TankkaartException(string message):base(message)
+        {
+            
+        }
+
+        public TankkaartException(string message, Exception innerException): base(message, innerException)
+        {
+            
+        }
+    }
+}
