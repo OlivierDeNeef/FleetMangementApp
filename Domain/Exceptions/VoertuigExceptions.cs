99ea79cbd00b86fafcf0c8e670d224200fdb501c
Olvier De Neef <Olivier.dn.1999@gmail.com>
2021-10-01 14:39:33 +0200

    Added VoertuigException

---
+++ Domain/Exceptions/VoertuigExceptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DomainLayer.Exceptions
+{
+    public class VoertuigExceptions : Exception
+    {
+
+        public VoertuigExceptions()
+        {
+            
+        }
+
+        public VoertuigExceptions(string message) : base(message)
+        {
+            
+        }
+
+        public VoertuigExceptions(string message, Exception innerException) : base(message,innerException)
+        {
+            
+        }
+    }
+}
