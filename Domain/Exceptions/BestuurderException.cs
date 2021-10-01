d9e06844ae6e27bbe9beabc0127d279a812a7810
Olvier De Neef <Olivier.dn.1999@gmail.com>
2021-10-01 14:38:41 +0200

    Added BestuurderException

---
+++ Domain/Exceptions/BestuurderException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DomainLayer.Exceptions
+{
+    public class BestuurderException : Exception
+    {
+        public BestuurderException()
+        {
+            
+        }
+
+        public BestuurderException(string message) : base(message)
+        {
+            
+        }
+
+        public BestuurderException(string message, Exception innerException): base(message,innerException)
+        {
+            
+        }
+    }
+}
