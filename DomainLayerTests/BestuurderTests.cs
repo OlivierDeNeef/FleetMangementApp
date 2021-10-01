35364254eaf1b37fa7679f07633da65aa9dcf41b
Olvier De Neef <Olivier.dn.1999@gmail.com>
2021-10-01 18:10:26 +0200

    Added Testproject voor domain laag

---
+++ DomainLayerTests/BestuurderTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Xunit;
+
+namespace DomainLayerTests
+{
+    public class BestuurderTests
+    {
+        [Fact()]
+        public void SetIdTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact()]
+        public void SetNaamTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact()]
+        public void SetVoornaamTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact()]
+        public void SetGeboortedatumTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact()]
+        public void HasRijbewijsTypeTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact()]
+        public void AddRijbewijsTypeTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact()]
+        public void RemoveRijbewijsTypeTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact()]
+        public void SetStraatTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact()]
+        public void SetHuisnummerTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact()]
+        public void SetStadTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact()]
+        public void SetLandTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact()]
+        public void SetTankkaartTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact()]
+        public void SetVoertuigTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact()]
+        public void SetDeletedTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
