bdb43bbc5a2d522433dab700efbd017a1d7e796a
Luca <luca.joos@student.hogent.be>
2021-10-06 17:32:13 +0200

    Update

---
+++ DomainLayerTests/VoertuigTests.cs
@@ -0,0 +1,79 @@
+﻿using Xunit;
+using DomainLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Tests
+{
+    public class VoertuigTests
+    {
+        [Fact()]
+        public void SetIdTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void SetMerkTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void SetModelTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void SetChassisnummerTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void SetWagenTypeTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void SetBrandstofTypeTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void SetNummerplaatTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void SetKleurTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void SetAantalDeurenTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void SetBestuurderTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void SetIsDeletedTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
