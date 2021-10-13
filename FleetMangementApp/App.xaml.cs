717f7d0217e295595360ca8b904bcc490bd30aee
Olivier De Neef <71842536+Oli4dn@users.noreply.github.com>
2021-10-13 21:08:49 +0200

    Upset dip

--- FleetMangementApp/App.xaml.cs
+++ FleetMangementApp/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using DomainLayer.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace FleetMangementApp
 {
@@ -13,5 +15,26 @@ namespace FleetMangementApp
     /// </summary>
     public partial class App : Application
     {
+        private ServiceProvider _serviceProvider;
+
+        public App()
+        {
+            ServiceCollection services = new ServiceCollection();
+            ConfigureServices(services);
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddSingleton<IBestuurderRepo>();
+            services.AddSingleton<IVoertuigRepo>();
+            services.AddSingleton<ITankkaartRepo>();
+            services.AddSingleton<IBrandstofTypeRepo>();
+            services.AddSingleton<IRijbewijsTypeRepo>();
+            services.AddSingleton<IWagenTypeRepo>();
+
+
+
+        }
     }
 }
