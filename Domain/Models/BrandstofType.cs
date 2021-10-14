d420fff8c3150a848364b5f7a0d44c324e96f102
Olivier De Neef <71842536+Oli4dn@users.noreply.github.com>
2021-10-14 09:31:37 +0200

    Update

--- Domain/Models/BrandstofType.cs
+++ Domain/Models/BrandstofType.cs
@@ -1,8 +1,30 @@
-﻿namespace DomainLayer.Models
+﻿using DomainLayer.Exceptions.Models;
+
+namespace DomainLayer.Models
 {
     public class BrandstofType
     {
-        public int Id { get; set; }
-        public string Type { get; set; }
+        public int Id { get; private set; }
+        public string Type { get; private set; }
+        
+
+        /// <summary>
+        /// Dit veranderd de id van het brandstofType
+        /// controlleer of het id groter is dan 1
+        /// </summary>
+        /// <param name="id"></param>
+        public void ZetId(int id)
+        {
+            if (id < 1) throw new BrandstofTypeException("ZetId - id < 1");
+            this.Id = id;
+        }
+
+        public void ZetType(string type)
+        {
+            if (string.IsNullOrWhiteSpace(type)) throw new BrandstofTypeException("ZetType - Type is null of leeg");
+            this.Type = type;
+        }
     }
+
+    
 }
