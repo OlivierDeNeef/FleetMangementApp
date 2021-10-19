8cfa9667cde75582a2f3810179a5e21619b9faad
Olivier De Neef <71842536+Oli4dn@users.noreply.github.com>
2021-10-19 17:54:02 +0200

    update

--- Domain/Managers/BrandstofTypeManager.cs
+++ Domain/Managers/BrandstofTypeManager.cs
@@ -16,6 +16,10 @@ namespace DomainLayer.Managers
             _brandstofTypeRepo = brandstofTypeRepo;
         }
 
+        /// <summary>
+        /// Voeg een brandstof type aan de data source
+        /// </summary>
+        /// <param name="brandstofType">Brandstof dat toegevoegd moet worden</param>
         public void VoegBrandstofTypeToe(BrandstofType brandstofType)
         {
             try
@@ -27,11 +31,15 @@ namespace DomainLayer.Managers
             }
             catch (Exception e)
             {
-               
                 throw new BrandstofTypeManagerException("VoegBrandstofTypeToe - Er ging iets mis bij het toevoegen", e);
             }
         }
 
+
+        /// <summary>
+        /// Verwijder een brandstof type van de data source
+        /// </summary>
+        /// <param name="brandstofType">Brandstof dat verwijderd moet worden</param>
         public void VerwijderBrandstofType(BrandstofType brandstofType)
         {
             try
@@ -47,6 +55,10 @@ namespace DomainLayer.Managers
             }
         }
 
+        /// <summary>
+        /// Update een brandstof in de data source
+        /// </summary>
+        /// <param name="brandstofType">Brandstof dat geupdate moet worden</param>
         public void UpdateBrandstofType(BrandstofType brandstofType)
         {
             try
@@ -62,6 +74,7 @@ namespace DomainLayer.Managers
             }
         }
 
+    
         public IEnumerable<BrandstofType> GeefAlleBrandstofTypes()
         {
             try
