641d0bc012bceb86495de109731d593de66c5189
Arnout Droesbeke <arnout.droesbeke@student.hogent.be>
2021-10-21 11:33:40 +0200

    appsettings configuration

--- FleetMangementApp/App.xaml.cs
+++ FleetMangementApp/App.xaml.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using DataAccessLayer.Repos;
 using DomainLayer.Interfaces;
 using DomainLayer.Interfaces.Repos;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
 using Microsoft.Extensions.DependencyInjection;
+
 
 namespace FleetMangementApp
 {
@@ -17,21 +22,26 @@ namespace FleetMangementApp
     public partial class App : Application
     {
         private ServiceProvider _serviceProvider;
+        private readonly IConfiguration configuration;
 
         public App()
         {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional : false, reloadOnChange : true);
+            configuration = builder.Build();
             ServiceCollection services = new ServiceCollection();
             ConfigureServices(services);
             _serviceProvider = services.BuildServiceProvider();
         }
 
+
         private void ConfigureServices(ServiceCollection services)
         {
+            
             services.AddSingleton<IBestuurderRepo>();
             services.AddSingleton<IVoertuigRepo>();
             services.AddSingleton<ITankkaartRepo>();
             services.AddSingleton<IBrandstofTypeRepo>();
-            services.AddSingleton<IRijbewijsTypeRepo>();
+            services.AddSingleton<IRijbewijsTypeRepo, RijbewijsTypeRepo>();
             services.AddSingleton<IWagenTypeRepo>();
 
 
