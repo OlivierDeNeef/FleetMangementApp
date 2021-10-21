505b701f0af072ba5983c228a754a9562bb5fbb6
Luca <luca.joos@student.hogent.be>
2021-10-21 09:52:29 +0200

    update

--- Domain/Models/WagenType.cs
+++ Domain/Models/WagenType.cs
@@ -16,13 +16,21 @@ namespace DomainLayer.Models
             ZetId(id);
             ZetType(type);
         }
-
+        /// <summary>
+        /// zet het id van het wagentype
+        /// </summary>
+        /// <param name="id">WagentypeId</param>
         public void ZetId(int id)
         {
             if (id <= 0) throw new WagenTypeException("ZetId - id moet groter zijn dan 0");
             Id = id;
         }
 
+
+        /// <summary>
+        /// Zet het type van het wagentype
+        /// </summary>
+        /// <param name="type">Wagentype van de wagen</param>
         public void ZetType(string type)
         {
             if (string.IsNullOrWhiteSpace(type)) throw new WagenTypeException("ZetType - mag niet leeg zijn");
