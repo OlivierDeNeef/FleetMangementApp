2c7018b99e8f0224bb9352b1e5cc9be518a6c007
Olivier De Neef <71842536+Oli4dn@users.noreply.github.com>
2021-10-27 22:16:56 +0200

    update

---
+++ DataAccessLayer/Exceptions/Repos/WagenTypeRepoException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DataAccessLayer.Exceptions.Repos
+{
+    public class WagenTypeRepoException : Exception
+    {
+        public WagenTypeRepoException()
+        {
+            
+        }
+
+        public WagenTypeRepoException(string message) : base(message)
+        {
+            
+        }
+
+        public WagenTypeRepoException(string message, Exception innerException) : base(message, innerException)
+        {
+            
+        } 
+    }
+}
