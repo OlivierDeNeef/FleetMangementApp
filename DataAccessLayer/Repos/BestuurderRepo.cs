ce9412910f4e11bce45361dbfde30c6f4fae599e
Arnout Droesbeke <arnout.droesbeke@student.hogent.be>
2021-11-04 10:55:54 +0100

    aanmaking overige repos in datalaag

---
+++ DataAccessLayer/Repos/BestuurderRepo.cs
@@ -0,0 +1,39 @@
+﻿using DomainLayer.Interfaces.Repos;
+using DomainLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repos
+{
+    public class BestuurderRepo: IBestuurderRepo
+    {
+        public bool BestaatBestuurder(Bestuurder b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Bestuurder GeefBestuurder(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyList<Bestuurder> GeefGefilterdeBestuurders([Optional] int id,[Optional] string voornaam,[Optional] string naam,[Optional] DateTime geboortedatum,[Optional] List<RijbewijsType> lijstRijbewijstypes,[Optional] string rijksregisternummer,[Optional] bool gearchiveerd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBestuurder(Bestuurder b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void VoegBestuurderToe(Bestuurder b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccessLayer/Repos/VoertuigRepo.cs
@@ -0,0 +1,39 @@
+﻿using DomainLayer.Interfaces.Repos;
+using DomainLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repos
+{
+    public class VoertuigRepo: IVoertuigRepo
+    {
+        public void BestaatVoertuig(Voertuig voertuig)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyList<Voertuig> GeefGefilterdeVoertuigen([Optional] int id,[Optional] string merk,[Optional] string model,[Optional] int aantalDeuren,[Optional] string nummerplaat,[Optional] string chassisnummer,[Optional] string kleur,[Optional] WagenType wagenType,[Optional] BrandstofType brandstofType,[Optional] bool geachriveerd,[Optional] RijbewijsType type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Voertuig GeefTVoertuig(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateVoertuig(Voertuig voertuig)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void VoegWagenToe(Voertuig voertuig)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
