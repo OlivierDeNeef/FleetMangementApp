8b6ec2ecf1f31a5c764b4e5eb69508ed4cbd1a9c
Olivier De Neef <71842536+Oli4dn@users.noreply.github.com>
2021-11-18 10:40:54 +0100

    Update

--- DataAccessLayer/Repos/TankkaartRepo.cs
+++ DataAccessLayer/Repos/TankkaartRepo.cs
@@ -3,15 +3,10 @@ using DomainLayer.Models;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using DataAccessLayer.Exceptions.Repos;
-using DomainLayer.Exceptions.Managers;
 
 namespace DataAccessLayer.Repos
 {
