38e5e9db083a0030061f747e7e893161dd5e1c88
Olivier De Neef <71842536+Oli4dn@users.noreply.github.com>
2022-01-08 23:23:36 +0100

    Update

---
+++ DataGenerator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DataGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+               
+            }
+        }
+    }
+}
